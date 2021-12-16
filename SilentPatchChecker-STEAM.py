1a801a4218023bedd7bf7a59f2166fcc4aa6ee12
amzy <hello@amzy.live>
2021-12-16 22:21:49 +0000

    added all files in a list
    exe will come soon when i figure out how not to break it

--- SilentPatchChecker-STEAM.py
+++ SilentPatchChecker-STEAM.py
@@ -1,32 +1,37 @@
 ###############################################
-#WINDOWS VER 0.8
-#CREATED BY GITHUB.COM/XAMZY
+#WINDOWS VER 1.0
+#GITHUB.COM/XAMZY
 ###############################################
 
 #imports exists for current method, winreg for new unfinished method, and time for another unused method
-from os.path import exists
 import os
 import winreg
 import time
 import itertools
 
 #locates Bully install directory from registry and stores it in key
-test = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 12200")
-key = winreg.QueryValueEx(test, r"InstallLocation")
+locate = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 12200")
+key = winreg.QueryValueEx(locate, r"InstallLocation")
 
 #changes directory to stored location in key and checks if Bully.exe and SilentPatchBully.asi are in said directory with no folders seperating them
 os.chdir(key[0])
-file_existsBL = exists('Bully.exe')
-file_existsSP = exists('SilentPatchBully.asi')
 
-#if both files exist, return true that SPBully is installed correctly // else if a file is missing(aka, not both found) then return false 
-if file_existsSP and file_existsBL:
+#creates a list of all the files required for SPBully
+filelist = ["Bully.exe",
+            "dinput8.dll",
+            "MiniDumper.asi",
+            "modupdater.asi",
+            "modupdater.ini",
+            "SilentPatchBully.asi",
+            "SilentPatchBully.ini"]
+
+#if any of these files are missing, tells you where to put them.
+for file in filelist:
+    if not os.path.exists(file):
+        print(f"SilentPatchBully is NOT installed correctly. Place all files in the main Bully Directory.\nYou can find the correct location here: {key[0]}")
+        break
+#else if all files exist, print true that SPBully is installed correctly
+else:
     print("SilentPatchBully is installed in the correct directory")
-    input()
-#else if any of these files are missing, tells you where to put them.
-elif not file_existsSP or not file_existsBL:
-    print("SilentPatchBully is NOT installed correctly.. Place all files in the main Bully Directory. You can find the correct location here: ")
-    #test = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 12200")
-    #key = winreg.QueryValueEx(test, r"InstallLocation")
-    print(key[0])
-input()
+
+input("\n You may now close this program.")
